b20d1093da7376f57b00a8dfa0c836fbc3ba2aa3
Henrik Bak Thomassen <henrik@createinside.com>
2012-10-24 14:07:11 +0200

    Added js mobile detection

--- sites/all/themes/netstof/scripts/settings.js
+++ sites/all/themes/netstof/scripts/settings.js
@@ -144,8 +144,9 @@ $(function() {
     /**
      * ATTACH TOOLTIPS
      */
-    attachTooltips();
-    
+    if( !isMobile.any() ) {
+    	attachTooltips();
+    }
     /**
      * ATTACH WATERMARKS
      */
@@ -153,7 +154,9 @@ $(function() {
     
     // Ajax complete
     $(document).ajaxComplete(function() {
-    	attachTooltips();
+      if( !isMobile.any() ) {
+    		attachTooltips();
+		}
     	attachWatermarks();
     });
     
@@ -206,3 +209,24 @@ function attachWatermarks() {
 	$("#webform-client-form-564 #edit-submitted-din-e-mail").watermark("Email", {useNative: false});
 	$("#webform-client-form-564 #edit-submitted-indhold").watermark("Indhold", {useNative: false});
 }
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
