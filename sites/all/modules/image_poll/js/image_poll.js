29733ee4711b7f1078c52ef6fbba999549aa3556
DanielMM <moisdaniel7@gmail.com>
2014-01-28 13:55:23 +0100

    Migrate minipoll JS from uddannelsesuge.

--- sites/all/modules/image_poll/js/image_poll.js
+++ sites/all/modules/image_poll/js/image_poll.js
@@ -1,26 +1,31 @@
 (function ($) {
     Drupal.behaviors.YourBehaviour = {
-    attach: function() {
+    attach: function() {  
+        // Trigger radiobtn
+        $('.poll.image_poll .choice').bind('touchstart mousedown', function(e){
+            e.preventDefault();
+            $(this).find('label').prop("checked", true).trigger("click");
+        });
 
         // Reveal extra info field after casting the vote for first time users
         $('[id^=image_poll-form]').click(function(event){
-        
-            var mp_node = $(this).parents('.node-content');
-            mp_node.children('.field-name-field-extra-info').slideDown();
-        });
 
+            var mp_node = $(this).parents('.inner-wrapper');
+            mp_node.children('.extra-info-wrapper').slideDown();
+        });
+        
         // When changing votes, first the vote is canceled and then
-        // submit the other choice programatically 
+        // submit the other choice programatically
         $('[id^=image_poll-form]').ajaxComplete(function(event, xhr, settings) {
             var formid = Drupal.settings.image_poll.formid;
 
             $(this).find('input[value^=' + formid + ']').mousedown();
-            
+
         // Reveal extra info field after changing the vote
-            var mp_node = $('input[value^=' + formid + ']').parents('.node-content');
-            mp_node.children('.field-name-field-extra-info').slideDown();
+            var mp_node = $('input[value^=' + formid + ']').parents('.inner-wrapper');
+            mp_node.children('.extra-info-wrapper').slideDown();
         });
     }
-    
+ 
     }
 })(jQuery);
