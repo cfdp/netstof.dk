b372fb906acb35ec53d9bf7390beec3247cdff64
Michael Lajlev <lajlev@gmail.com>
2014-03-05 14:44:27 +0100

    læs mere knap skjult fra afstemninger

--- sites/all/modules/image_poll/templates/image_poll-choice-form.tpl.php
+++ sites/all/modules/image_poll/templates/image_poll-choice-form.tpl.php
@@ -51,19 +51,3 @@
   <?php print $hidden; ?>
   </div>
 </div>
-<?php
-  $node = node_load($nid);
-  $sharing_url = "http://netstof.dk/node/" . $nid;
-  $sharing_text = "Afstemning: $node->title";
-  $sharing_text_encoded = urlencode($sharing_text);
-?>
-<div id="share-buttons">
-  <!-- Facebook -->
-  <a href="http://www.facebook.com/sharer.php?u=<?php print $sharing_url; ?>" target="_blank"><img src="/<?php print drupal_get_path('module', 'image_poll'); ?>/img/social/facebook.png" alt="Facebook" /></a>
-  <!-- Twitter -->
-  <a href="http://twitter.com/share?url=<?php print $sharing_url; ?>&text=<?php print $sharing_text_encoded; ?>" target="_blank"><img src="/<?php print drupal_get_path('module', 'image_poll'); ?>/img/social/twitter.png" alt="Twitter" /></a>
-  <!-- Google+ -->
-  <a href="https://plus.google.com/share?url=<?php print $sharing_url; ?>" target="_blank"><img src="/<?php print drupal_get_path('module', 'image_poll'); ?>/img/social/google.png" alt="Google" /></a>
-  <!-- Email -->
-  <a href="mailto:?Subject=<?php print $sharing_text_encoded; ?>&Body=%20<?php print $sharing_url; ?>"><img src="/<?php print drupal_get_path('module', 'image_poll'); ?>/img/social/email.png" alt="Email" /></a>
-</div>
