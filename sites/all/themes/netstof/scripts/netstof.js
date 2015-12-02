dc29e2efce1ed754e104c28dee60eb4fbaf8cefe
CfDP <tech@cfdp.dk>
2015-12-02 12:57:42 +0100

    Fix search auto submit issue on redirect DK-map pages

--- sites/all/themes/netstof/scripts/netstof.js
+++ sites/all/themes/netstof/scripts/netstof.js
@@ -1,17 +1,32 @@
 (function($) {
+  /* Redirects user to a custom municipality page */
   Drupal.behaviors.DKmapRedirect = {
     attach: function (context, settings) {
-      /* Only add the functionality on the relevant pages */
+      /* Only add the functionality once on the relevant pages */
       $('.page-node-569', context).once('DKMapRedirect', function () {
         var path_params = $(location).attr('search');
         var path_hostname = $(location).attr('hostname');
         var path_name= $(location).attr('pathname');
         if (path_params.substring(9,12) == "318") {
-          console.log(path_hostname+path_name+"/kommuner/slagelse");
           window.location.replace("/kommuner/slagelse"+path_params);
         }
       });
     }
   };
-  
+  /* Shows text tip in search box */
+  Drupal.behaviors.searchTip = {
+    attach: function (context, settings) {
+      /* Only add the functionality once */
+      $('.html', context).once('searchTip', function () {
+        $( ".form-item-search-block-form input" ).val('Søg på netstof.dk');
+        $( ".form-item-search-block-form input" ).focus(function() {
+          if (this.value == 'Søg på netstof.dk') {this.value = '';}
+        });
+        $( ".form-item-search-block-form input" ).blur(function() {
+          if (this.value == '') {this.value = 'Søg på netstof.dk';}
+        });
+      });
+    }
+  };
+
 })(jQuery);
